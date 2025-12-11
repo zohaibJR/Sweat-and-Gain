b6537cbf5b753f7993f92e4b8b1c98ac26ab3152
Zohaib UR Rehman <100087461+zohaibJR@users.noreply.github.com>
2025-12-11 22:10:25 +0500

    Set till Quaotes
    Set till Quaotes

---
+++ gymfitnessfrontend/src/Components/QuickView/QuickView.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../QuickView/QuickView.css'
+
+function QuickView() {
+  return (
+    <div className='QuickView'>
+      <h1>Quick View</h1>
+    </div>
+  )
+}
+
+export default QuickView
---
+++ gymfitnessfrontend/src/Components/Quote/Quote.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Quote.css'
+
+function Quote() {
+  return (
+    <div className='MainQuote'>
+      <h2>No Pain, No Gain</h2>
+    </div>
+  )
+}
+
+export default Quote
